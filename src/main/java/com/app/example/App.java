e714a7612cdf220e7ef11069b03eec8a7532a130
Ankur Mangal <ankur.mangal24@gmail.com>
2023-01-12 16:12:43 +0530

    Update App.java

--- src/main/java/com/app/example/App.java
+++ src/main/java/com/app/example/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
    @RequestMapping(value = "/")
    public String hello() {
-      return "<center>Hello Welcome to demo By ankur using Jenkins</center>";
+      return "<center>Hello Welcome to demo By ankur using azure pipleine</center>";
    }
 }
